a7738680e9a6a8b80daf980d1e1827e94e1390be
voiladaniel <53874653+voiladaniel@users.noreply.github.com>
2020-04-09 17:17:51 +0300

    increase speed

--- src/Helpers/AttackerModal.js
+++ src/Helpers/AttackerModal.js
@@ -45,6 +45,17 @@ export const AttackerModal = ({ show, handleClose, handleChange, submitHandler, 
                   <option>7</option>
                   <option>8</option>
                   <option>9</option>
+                  <option>10</option>
+                  <option>11</option>           
+                  <option>12</option>
+                  <option>13</option>
+                  <option>14</option>
+                  <option>15</option>
+                  <option>16</option>
+                  <option>17</option>
+                  <option>18</option>
+                  <option>19</option>
+                  <option>20</option>
                 </Form.Control>
               </Form.Group>
             </Form.Row>
