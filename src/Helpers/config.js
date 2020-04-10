1744e8c7fa305ffc3027eae5bfa0f69db2f9e065
voiladaniel <53874653+voiladaniel@users.noreply.github.com>
2020-04-10 09:46:28 +0300

    udpate to azure

--- src/Helpers/config.js
+++ src/Helpers/config.js
@@ -1,4 +1,4 @@
 export const config = {
-    accountAPI: 'https://myapi.vdstuff.com/api/Account/Login',
-    templateAPI: 'https://myapi.vdstuff.com/api/Template/'
+    accountAPI: 'https://travianmanager.azurewebsites.net/api/Account/Login',
+    templateAPI: 'https://travianmanager.azurewebsites.net/api/Template/'
 };
