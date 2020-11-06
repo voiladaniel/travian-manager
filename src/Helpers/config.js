c6e5f117d18ab752bf083858781e37fe05e06432
voiladaniel <53874653+voiladaniel@users.noreply.github.com>
2020-11-06 16:20:05 +0200

    Update config.js

--- src/Helpers/config.js
+++ src/Helpers/config.js
@@ -1,4 +1,4 @@
 export const config = {
-    accountAPI: 'https://travianapi.vdstuff.com/api/Account/Login',
+    accountAPI: 'https://travianapi.vdstuff.com/api/Account/',
     templateAPI: 'https://travianapi.vdstuff.com/api/Template/'
 };
