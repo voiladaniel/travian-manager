03a43f824bf27aab548b46e6d8721abda7fd3d20
voiladaniel <53874653+voiladaniel@users.noreply.github.com>
2021-07-06 13:30:27 +0300

    Update config.js

--- src/Helpers/config.js
+++ src/Helpers/config.js
@@ -1,5 +1,5 @@
 export const config = {
-    accountAPI: 'https://travianapi.vdstuff.com/api/Account/',
+    accountAPI: 'http://tm-prod.us-east-2.elasticbeanstalk.com/api/Account/',
     templateAPI: 'https://travianapi.vdstuff.com/api/Template/',
     planAPI: 'https://travianapi.vdstuff.com/api/Plan/'
 };
